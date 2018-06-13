ec18af77966aab25fb2019c0cb2a0fd730431524
chenchen <chenchen17264@gmail.com>
2018-06-13 19:44:19 +0800

    队列也写完了哈哈哈“

--- 队列.cpp
+++ 队列.cpp
@@ -14,11 +14,54 @@ public:
 		data=a;
 		next=ptr;
 	}
-}
+};
 
-template Queue:public DNode<T>
+template <class T>
+class Queue:public DNode<T>
 {
 private:
-	
+	DNode<T>* front;
+	DNode<T>* rear;
+	int size;
+public:
+	Queue();
+	Queue(const T &item) { rear = new DNode<T>(item); }
+	Queue(const Queue<T> &a);
+	~Queue();
+	void Clear();
+	DNode<T>* Locate(int pos);
+	void QueueInsert(const T &a);
+	void QueueDelete();
+	void Queuechange(const T &a, int pos);
+	Queue<T> &operator =(const Queue<T> &a);
+	void Queuedisplay();	
+};
+
+template <class T>
+void Queue<T>::QueueInsert(const T &a)
+{
+	DNode<T>* temp=new DNode<T>;
+	temp->data=a;
+	temp->next=NULL;
+	if(tail=NULL)
+	{
+		front=temp;
+	}
+	else
+		tail->next=temp;
+	rear=temp;
+	size++;
 }
 
+template <class T>
+void Queue<T>::QueueDelete()
+{
+	DNode<T>* temp;
+	if(size==0) return;
+	temp=front;
+	front=front->next;
+	delete temp;
+	if(front==NULL)
+		rear=NULL;
+	size--;
+}
