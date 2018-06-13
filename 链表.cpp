621c4f88c21e6bfe2b74980750a915e7c968c16f
chenchen <chenchen17264@gmail.com>
2018-06-13 11:29:09 +0800

    new

--- 链表.cpp
+++ 链表.cpp
@@ -4,30 +4,40 @@
 using namespace std;
 
 template <class T>
-class LinkedList
-{
-private:
-	template <class T>
 	class LNode
 	{
 	public:
 		T data;
 		LNode* next;
+		LNode(LNode<T> *ptr = NULL) { next = ptr; }
+		LNode(const T &item, LNode<T> *ptr = NULL)
+		{
+			next = ptr;
+			data = item;
+		}
 	};
+
+template <class T>
+class LinkedList:public LNode<T>
+{
+private:
+	
 	LNode<T> *head;
 	LNode<T> *tail;
 	LNode<T> *cur;
 	int size;
 public:
 	LinkedList();
+	LinkedList(const T &item) { head = new LNode<T>(item); }
 	LinkedList(const LinkedList<T> &a);
 	~LinkedList();
 	void Clear();
-	LinkedList<T>* Locate(int pos);
-	void LinkInsert(T &a, int pos);
-	void LinkDelete(T &a,int pos);
+	LNode<T>* Locate(int pos);
+	void LinkInsert(const T &a, int pos);
+	void LinkDelete(T &a, int pos);
 	void Linkchange(T &a, int pos);
 	LinkedList<T> &operator =(const LinkedList<T> &a);
+	void Linkdisplay();
 };
 
 template <class T>
@@ -44,14 +54,14 @@ LinkedList<T>::LinkedList(const LinkedList<T> &a)
 {
 	int n = a.size;
 	size = n;
-	head = new LNode<T>;
+	head = new LNode<T>(a.head->data);
 	LNode<T> *temp1 = head;
 	LNode<T> *temp2 = a.head;
-	while (n--)
+	for(int i=0;i<n;i++)
 	{
-		temp1->data = temp2->data;
-		temp1 = temp1->next;
 		temp2 = temp2->next;
+		LinkInsert(temp2->data, i);
+		
 	}
 }
 
@@ -78,7 +88,8 @@ LinkedList<T>::~LinkedList()
 }
 
 template <class T>
-LinkedList<T>* LinkedList<T>::Locate(int pos)
+
+LNode<T>* LinkedList<T>::Locate(int pos)
 {
 	int i = 0;
 	LNode<T>* temp = head;
@@ -93,13 +104,27 @@ LinkedList<T>* LinkedList<T>::Locate(int pos)
 }
 
 template <class T>
-void LinkedList<T>::LinkInsert(T &a,int pos)
+void LinkedList<T>::LinkInsert(const T &a,int pos)
 {
 	LNode<T> *p = Locate(pos);
-	LNode<T> *temp = new LNode<T>(a);
-	temp->next = p->next;
-	p->next = temp;
-	size++;
+	if (p == NULL)
+	{
+		if (p == head)
+		{
+			head = new LNode<T>;
+			head->data = a;
+		}
+			
+		else return;
+
+	}
+	else
+	{
+		LNode<T> *temp = new LNode<T>(a);
+		temp->next = p->next;
+		p->next = temp;
+		size++;
+	}
 }
 
 template <class T>
@@ -127,7 +152,7 @@ template <class T>
 LinkedList<T>& LinkedList<T>::operator =(const LinkedList<T> &a)
 {
 	int n = a.size;
-	clear();
+	Clear();
 	head = new LNode<T>;
 	LNode<T> *temp1 = head;
 	LNode<T> *temp2 = a.head;
@@ -139,7 +164,28 @@ LinkedList<T>& LinkedList<T>::operator =(const LinkedList<T> &a)
 	}
 }
 
+template <class T>
+void LinkedList<T>::Linkdisplay()
+{
+	LNode<T>* temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+}
+
 int main()
 {
+	LinkedList<int> l1;
+	for (int i = 0; i < 15; i++)
+	{
+		l1.LinkInsert(rand(), 0);
+	}
+	l1.Linkdisplay();
+	cout << endl;
+	LinkedList<int> l2(l1);
+	l2.Linkdisplay();
+	cin.get();
 	return 0;
 }
